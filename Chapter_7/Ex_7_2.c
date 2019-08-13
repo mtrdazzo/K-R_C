8a3dc0ea594f6aecfe1bc407488d19f3c6c82adc
Matt <mtrdazzo@gmail.com>
2019-08-12 22:16:23 -0700

    Initial commit of Ch7 Ex2

---
+++ Chapter_7/Ex_7_2.c
@@ -0,0 +1,16 @@
+/*
+ *
+ * Exercise 7-2
+ * 
+ * Write a program that will print arbitrary input in a sensible way. As a minimum, it should print 
+ * non-graphic characters in octal or hexadecimal according to a local custom, and break long text
+ * lines.
+ * 
+ */
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    return 0;
+}
